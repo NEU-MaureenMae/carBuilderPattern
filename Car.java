533bc16dd4b0b01bc83051735886850ec5203461
Maureen-Mae B. Sebastian <142388792+NEU-MaureenMae@users.noreply.github.com>
2024-02-22 06:44:32 +0000

    update

---
+++ Car.java
@@ -0,0 +1,67 @@
+public class Car{
+    private final String brand, engineType, tranmission, color;
+    private final int passengerCap;
+
+    private Car(CarBuilder builder) {
+        this.brand = builder.brand;
+        this.engineType = builder.engineType;
+        this.transmission = builder.transmission;
+        this.color = builder.color;
+        this.passengerCap = builder.passengerCap;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public String getPassengerCap(){
+        return passengerCap;
+    }
+
+    private static class CarBuilder(){
+        private final String brand, engineType, tranmission, color;
+        private final int passengerCap; 
+
+        public CarBuilder brand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public CarBuilder engineType(String engineType) {
+            this.engineType = engineType;
+            return this;
+        }
+
+        public CarBuilder transmission(String transmission) {
+            this.transmission = transmission;
+            return this;
+        }
+
+        public CarBuilder color(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public CarBuilder passengerCap(int passengerCap) {
+            this.passengerCap = passengerCap;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main(){
+    public static void main(String[] args) {
+        Car car = new car.CarBuilder()
+                this.brand("Honda");
+                this.engineType("gasoline");
+                this.transmission("automatic");
+                this.color("blue");
+                this.passengerCap(23);
+
+                
+}
